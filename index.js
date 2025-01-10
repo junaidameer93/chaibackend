4f63f71f673df1aa09c4851876b9de8e0a2bc580
junaidameer93 <junaid.ameer93@gmail.com>
2025-01-10 15:57:15 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About Us')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>Login here</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}!`) 
+})
