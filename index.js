4ef722937886a008995c4ba023bb428b3e9af57d
junaidameer93 <junaid.ameer93@gmail.com>
2025-01-14 12:53:14 +0500

    user json

--- index.js
+++ index.js
@@ -18,6 +18,24 @@ app.get('/register', (req, res) => {
     res.send('<h1>Register here</h1>')
 })
 
+const user = {
+  id: 1,
+  name: "John Doe",
+  email: "john.doe@example.com",
+  age: 29,
+  isActive: true,
+  roles: ["user", "admin"],
+  address: {
+    street: "123 Main Street",
+    city: "New York",
+    state: "NY",
+    zip: "10001"
+  }
+};
+
+app.get('/user', (req, res) => {
+  res.json(user);
+})
 //test comiit 222
 
 app.listen(process.env.PORT, () => {
